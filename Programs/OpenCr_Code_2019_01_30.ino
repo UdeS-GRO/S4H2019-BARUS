49d39839b4fb294c070af9618f0052f1d36c92de
alexandre demers <46566649+AlexandreDemersCyracus@users.noreply.github.com>
2019-02-06 09:36:19 -0500

    Update OpenCr_Code_2019_01_30.ino

--- Programs/OpenCr_Code_2019_01_30.ino
+++ Programs/OpenCr_Code_2019_01_30.ino
@@ -1,18 +1,10 @@
-/*******************************************************************************
-* Copyright 2016 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Program Name		  : InitiateMotor.py
+//Date of creation	: 2019/01/30
+//Creator		        : BARUS team members - Olivier Girouard, Alexandre Demers
+//
+//Description		    : The program intiate 3 motors in Joint Mode and make them mode at the and to make sure the communication is OK
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* Authors: Olivier Girouard, Alexandre Demers */
 
