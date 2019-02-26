e64881dcc83f687fa26d16d9f2d3d05b9384fc86
Moise2901 <46569983+Moise2901@users.noreply.github.com>
2019-02-26 14:16:44 -0500

    Update Motor.py

--- BARUS_CODE/BARUS_RPI_CODE/Motor.py
+++ BARUS_CODE/BARUS_RPI_CODE/Motor.py
@@ -9,8 +9,8 @@ def moveMotor(motorId, position):
     SerialCom.sendIntToArduino(motorId, position)
 
 def readMotorPosition(motorId):
-    SerialCom.sendIntToArduino(50000 + motorId)
-    position = SerialCom.receiveIntFromArduino
+    SerialCom.sendIntToArduino(motorId)
+    position = SerialCom.receiveIntFromArduino()
     while position < 0 or position > 4092:
-        position = SerialCom.receiveIntFromArduino
+        position = SerialCom.receiveIntFromArduino()
     return position
