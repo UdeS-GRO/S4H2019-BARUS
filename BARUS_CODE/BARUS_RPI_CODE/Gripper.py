c2f6bd3a4b1b7f405fb693f76ea2577e7d33076f
Moise2901 <46569983+Moise2901@users.noreply.github.com>
2019-02-26 14:26:59 -0500

    Update Gripper.py

--- BARUS_CODE/BARUS_RPI_CODE/Gripper.py
+++ BARUS_CODE/BARUS_RPI_CODE/Gripper.py
@@ -1,16 +1,20 @@
 import SerialCom
 
-gripperSignal = 0
+#gripperSignal = 0
 openSignal = 1
 closeSignal = 0
 
 
+#def openGripper():
+    #msg = 10000 * gripperSignal + openSignal
+    #SerialCom.sendIntToArduino(msg)
+#def closeGripper():
+    #msg = 10000 * gripperSignal + closeSignal
+    #SerialCom.sendIntToArduino(msg)
 def closeGripper():
-    msg = 10000 * gripperSignal + closeSignal
-    SerialCom.sendIntToArduino(msg)
-
-
+    SerialCom.sendIntToArduino(closeSignal)
 def openGripper():
-    msg = 10000 * gripperSignal + openSignal
-    SerialCom.sendIntToArduino(msg)
+    SerialCom.sendIntToArduino(openSignal)
+
+
 
